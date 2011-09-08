264805cf773fbcda9ea3fe1dc2854ad3bf9ea469
Avdi Grimm <avdi@avdi.org>
2011-09-08 16:01:38 -0400

    Set up 'custom' file name.

--- init.el
+++ init.el
@@ -1,1 +1,8 @@
 ; Avdi Grimm's Emacs 24 Configuration
+(setq abg-emacs-init-file load-file-name)
+(setq abg-emacs-config-dir
+      (file-name-directory abg-emacs-init-file))
+
+;; Set up 'custom' system
+(setq custom-file (expand-file-name "emacs-customizations.el" abg-emacs-config-dir))
+(load custom-file)
