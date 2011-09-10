82ba7361eb10c3b36c3d9384075409d4833c6170
Avdi Grimm <avdi@avdi.org>
2011-09-10 08:03:30 -0400

    Set backup directory

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 (setq abg-emacs-config-dir
       (file-name-directory abg-emacs-init-file))
 
+(setq user-emacs-directory abg-emacs-config-dir)
+
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
+
+
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" abg-emacs-config-dir))
 (load custom-file)
+
