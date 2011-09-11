107287fc668f6d1abc78c1787c0b5035559d64ea
Avdi Grimm <avdi@avdi.org>
2011-09-11 13:54:47 -0400

    Add marmalade archive.

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
