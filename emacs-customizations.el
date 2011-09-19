f7be701abd9cb9e4576b1382e6d40ea5f14c1b54
Avdi Grimm <avdi@avdi.org>
2011-09-19 15:11:06 -0400

    Globally enable ido for buffer and file switching.

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
  '(tool-bar-mode nil))
 (custom-set-faces
