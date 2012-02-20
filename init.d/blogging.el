d9a01e673df21f3d87acfe7bd65d2238cdbc4082
Avdi Grimm <avdi@avdi.org>
2012-02-20 15:25:01 -0500

    Add CodeBenders blog info.

--- init.d/blogging.el
+++ init.d/blogging.el
@@ -1,10 +1,16 @@
 (require 'netrc)
 (require 'org2blog)
 (setq abg-netrc-vc (netrc-machine (netrc-parse "~/.netrc") "virtuouscode" t))
+(setq abg-netrc-cb (netrc-machine (netrc-parse "~/.netrc") "codebenders" t))
 (setq org2blog/wp-blog-alist
       '(("virtuouscode"
 	 :url "http://avdi.org/devblog/xmlrpc.php"
 	 :username (netrc-get abg-netrc-vc "login")
 	 :password (netrc-get abg-netrc-vc "password")
+	 :tags-as-categories nil)
+	("codebenders"
+	 :url "http://www.codebenders.com/xmlrpc.php"
+	 :username (netrc-get abg-netrc-cb "login")
+	 :password (netrc-get abg-netrc-cb "password")
 	 :tags-as-categories nil)))
 
