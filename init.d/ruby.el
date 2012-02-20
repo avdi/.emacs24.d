eb96c428772ef8b2df47b06b755da0e4424061de
Avdi Grimm <avdi@avdi.org>
2012-02-20 15:24:29 -0500

    Load rcodetools from gem. Stop activating RVM with every file loaded.

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -1,5 +1,17 @@
 (add-hook 'ruby-mode-hook
 	  (lambda () (run-hooks 'abg-code-modes-hook)))
-(add-hook 'ruby-mode-hook
-	  (lambda () (rvm-activate-corresponding-ruby)))
+;(add-hook 'ruby-mode-hook
+;	  (lambda () (rvm-activate-corresponding-ruby)))
+
+;; Load rcodetools
+(rvm-use-default)
+
+(defun abg-rcodetools-path ()
+  (let ((gem-output (shell-command-to-string "gem list -l rcodetools"))
+	(version-pattern (rx ?( (group (* anything)) ?))))
+    (string-match version-pattern gem-output)
+    (let ((version (match-string-no-properties 1 gem-output)))
+      (concat (rvm--emacs-gemhome) "/gems/rcodetools-" version "/rcodetools.el"))))
+
+(load (abg-rcodetools-path))
 
