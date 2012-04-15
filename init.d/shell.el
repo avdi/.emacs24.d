0ae427a7eaf07477987dc8bd3bf0e05a63446da2
Avdi Grimm <avdi@avdi.org>
2012-04-14 21:44:59 -0400

    Configure multi-term.

---
+++ init.d/shell.el
@@ -0,0 +1,21 @@
+(require 'ansi-color)
+(require 'multi-term)
+
+;; Copy the (possibly themed) ansi-color-names-vector to the
+;; (annoyingly separate) ansi-term-color-vector
+(add-hook 'term-mode-hook
+          (lambda ()
+            (set-variable 'ansi-term-color-vector ansi-color-names-vector)
+            (local-set-key (kbd "<f2>") 'rename-buffer)))
+
+
+
+(defun abg-shell-init ()
+  (local-set-key (kbd "C-z") 'self-insert-command)
+  (local-set-key (kbd "<f2>") 'rename-buffer))
+
+(add-hook 'shell-mode-hook
+          'abg-shell-init)
+
+
+
