f986418e8db271db0c19ef5de87009ee888c1127
Avdi Grimm <avdi@avdi.org>
2012-04-14 21:48:01 -0400

    Unset C-z keybinding.

--- init.d/ZZ_keybindings.el
+++ init.d/ZZ_keybindings.el
@@ -2,7 +2,7 @@
 
 ;; I accidentally hit the following keys WAY too often.
 (global-unset-key (kbd "C-x C-c")) 
-(global-unset-key (kbd "C-x C-z"))
+(global-unset-key (kbd "C-z"))
 
 (global-set-key (kbd "<f8>") 'gist-region-or-buffer)
 (global-set-key (kbd "<f9>") 'org2blog/wp-new-entry)
