7da970c5a0dd440f21e5d05a66ece3a2a77ae7b1
Avdi Grimm <avdi@avdi.org>
2012-04-14 21:42:03 -0400

    Enable blinking cursor.

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode t)
  '(coffee-tab-width 2)
  '(column-number-mode t)
  '(ido-enable-flex-matching t)
