8493dd0a971e22adf4a04e5782e61932fbde7c99
Avdi Grimm <avdi@avdi.org>
2012-04-14 21:48:17 -0400

    Various F-key bindings.

--- init.d/ZZ_keybindings.el
+++ init.d/ZZ_keybindings.el
@@ -4,6 +4,15 @@
 (global-unset-key (kbd "C-x C-c")) 
 (global-unset-key (kbd "C-z"))
 
+
+(global-set-key (kbd "<f1>") 'multi-term-dedicated-toggle) ; open/close "main" terminal
+(global-set-key (kbd "M-<f1>") 'multi-term) ; new terminal
+(defun abg-revert-buffer-no-confirm () 
+  "Revert buffer with no confirmation"
+  (interactive)
+  (revert-buffer nil t))
+(global-set-key (kbd "<f5>") 'abg-revert-buffer-no-confirm)
+(global-set-key (kbd "<f6>") 'magit-status)
 (global-set-key (kbd "<f8>") 'gist-region-or-buffer)
 (global-set-key (kbd "<f9>") 'org2blog/wp-new-entry)
 (global-set-key (kbd "S-<f9>") 'org2blog/wp-post-buffer)
