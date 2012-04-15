089b899addc6a450f9b3defd640d314a97e8b35a
Avdi Grimm <avdi@avdi.org>
2012-04-14 21:15:43 -0400

    Theme and font.

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -26,4 +26,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "Inconsolata")))))
---
+++ init.d/10_theme.el
@@ -0,0 +1,1 @@
+(add-hook 'after-init-hook (lambda () (load-theme 'zenburn)))
