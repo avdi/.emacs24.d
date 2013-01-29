92ca110ea23df2792493db1348c8867eefd96f11
Avdi Grimm <avdi@avdi.org>
2013-01-29 17:56:35 -0500

    Install color-theme-mode

--- init.d/10_theme.el
+++ init.d/10_theme.el
@@ -2,7 +2,7 @@
 ;; coding. Also uses the newer theming system.
 (add-to-list 'el-get-sources
              '(:name zenburn-theme :type elpa))
-(el-get 'sync '(zenburn-theme))
+(el-get 'sync '(color-theme zenburn-theme))
 
 ;(add-hook 'after-init-hook 'color-theme-molokai)
 (add-hook 'after-init-hook (lambda () (load-theme 'zenburn)))
