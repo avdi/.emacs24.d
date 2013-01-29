7ebcda6d65a62005efc6aea98c980632278e19df
Avdi Grimm <avdi@avdi.org>
2013-01-29 18:00:13 -0500

    Install 'diminish' and update the diminished yasnippet appearance.

--- init.d/diminish.el
+++ init.d/diminish.el
@@ -1,7 +1,8 @@
+(abg-require-package 'diminish)
 (when (require 'diminish nil 'noerror)
   (eval-after-load "abbrev"
     '(diminish 'abbrev-mode "Ab"))
   (eval-after-load "yasnippet"
-    '(diminish 'yas/minor-mode "Y"))
+    '(diminish 'yas-minor-mode " Y"))
   (eval-after-load "centered-cursor-mode"
     '(diminish 'centered-cursor-mode "Ctr")))
